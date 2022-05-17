89f7d9ff80efca72250ed02997a6a3973f7acedd
Ahmed Magdy <ahmedmagdy41996@gmail.com>
2022-05-17 17:36:36 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CVTemplateBuilder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
